a3fb2f6c78cd5d23cb431f6df82af04bb8e3f6a4
Gabriel <rhernadnez028@west-mec.org>
2019-09-05 17:17:24 -0700

    Added images

---
+++ JS/booking.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let cabinInfo = [{
+    cabinName: "SloppyJoe",
+    cabinPrice: "$75",
+    cabinSq: "2100 sqft."
+},
+{
+    cabinName: "Big Bass",
+    cabinPrice: "$100",
+    cabinSq: "1900 sqft."
+}]
+
+let owners = [{
+    name: "Gabriel Hernandez",
+    skills: "HTML5",
+    portrait: `<img src="gabriel.jpg" />`
+},
+{
+    name: "Anthony Arias",
+    skills: "HTML5",
+    portrait: `<img src="anthony.jpeg" />`
+},{
+    name: `Emmanuel Cortes`,
+    skills: `HTML5`,
+    portrait: `<img src="emmanuel.jpg" />`
+}]
